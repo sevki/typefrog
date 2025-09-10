c438f1a4e1b03a7740c87a4fcf542dda50aab573
sevki <s@sevki.io>
2025-09-11 00:14:57 +0100

    fix: remove typeql vendored,fix flaky tests

--- src/compute.rs
+++ src/compute.rs
@@ -17,22 +17,23 @@ mod arity {
 }
 
 ascent! {
+    #[allow(non_snake_case)]
     // Facts prelude
     // TODO(sevki): use ascent_source!{} macro to split this out.
     relation owns(Interned<Atom>, Interned<Atom>);
     relation sub(Interned<Atom>, Interned<Atom>);
     relation value(Interned<Atom>, Interned<Atom>);
-    relation abstract_(Interned<Atom>);
+    relation abs(Interned<Atom>);
     relation attribute(Interned<Atom>);
     relation role(Interned<Atom>);
     relation entity(Interned<Atom>);
-    relation relation_(Interned<Atom>);
+    relation rel(Interned<Atom>);
     relation abstracts(Interned<Atom>, Interned<Atom>, Interned<Atom>);
     relation entities(Interned<Atom>, Interned<Atom>, Interned<Atom>);
     // Rules
     sub(grandparent,grandchild) <-- sub(grandparent,parent), sub(parent,grandchild);
     owns(owner,owned) <-- sub(grandparent,owner), owns(grandparent,owned), attribute(owned);
-    abstracts(name,field,type_) <-- abstract_(name), owns(name,field), attribute(field), value(field,type_);
+    abstracts(name,field,type_) <-- abs(name), owns(name,field), attribute(field), value(field,type_);
     entities(name,field,type_) <-- entity(name), owns(name,field), attribute(field), value(field,type_);
 }
 
@@ -53,7 +54,7 @@ pub fn compute(input: &str) -> Result<IR, String> {
                                     match fact {
                                         // these are type declarations, as far as I can tell, an object cannot be both an abstract and a concrete entity
                                         Fact::Relation(interned) => {
-                                            prog.relation_.push((interned,));
+                                            prog.rel.push((interned,));
                                         }
                                         Fact::Attribute(interned) => {
                                             prog.attribute.push((interned,));
@@ -65,7 +66,7 @@ pub fn compute(input: &str) -> Result<IR, String> {
                                             prog.entity.push((interned,));
                                         }
                                         Fact::Abstract(abs) => {
-                                            prog.abstract_.push((abs,));
+                                            prog.abs.push((abs,));
                                         }
                                         Fact::Value(ident, type_) => {
                                             prog.value.push((ident, type_))
@@ -112,22 +113,27 @@ pub fn compute(input: &str) -> Result<IR, String> {
     }
 
     // Create traits from abstracts
-    let traits: Vec<Trait> = abstract_map
+    let mut traits: Vec<Trait> = abstract_map
         .into_iter()
-        .map(|(name, fields)| Trait {
-            name: name.to_string(),
-            funcs: fields
+        .map(|(name, fields)| {
+            let mut funcs: Vec<Fn> = fields
                 .into_iter()
                 .map(|(field, type_)| Fn {
                     name: field.to_string(),
                     return_type: type_.to_string(),
                 })
-                .collect(),
+                .collect();
+            funcs.sort_by(|a, b| a.name.cmp(&b.name));
+            Trait {
+                name: name.to_string(),
+                funcs,
+            }
         })
         .collect();
+    traits.sort_by(|a, b| a.name.cmp(&b.name));
 
     // Create structs from entities (but exclude abstracts that also appear in entities)
-    let structs: Vec<Struct> = entity_map
+    let mut structs: Vec<Struct> = entity_map
         .into_iter()
         .filter_map(|(name, fields)| {
             // this can come in the form of a type_.kind or an annotation on a type.
@@ -135,22 +141,25 @@ pub fn compute(input: &str) -> Result<IR, String> {
             // `entity A @abstract;`
             // not
             // `abstract A;`
-            if prog.abstract_.iter().any(|(abs,)| abs == &name) {
+            if prog.abs.iter().any(|(abs,)| abs == &name) {
                 None
             } else {
+                let mut sorted_fields: Vec<Field> = fields
+                    .into_iter()
+                    .map(|(field, type_)| Field {
+                        name: field.to_string(),
+                        ty: type_.to_string(),
+                    })
+                    .collect();
+                sorted_fields.sort_by(|a, b| a.name.cmp(&b.name));
                 Some(Struct {
                     name: name.to_string(),
-                    fields: fields
-                        .into_iter()
-                        .map(|(field, type_)| Field {
-                            name: field.to_string(),
-                            ty: type_.to_string(),
-                        })
-                        .collect(),
+                    fields: sorted_fields,
                 })
             }
         })
         .collect();
+    structs.sort_by(|a, b| a.name.cmp(&b.name));
 
     Ok(IR { structs, traits })
 }
